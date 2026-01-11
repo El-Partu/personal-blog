6ffec502061336d3082430a15a81b67b0e86cca5
El-Partu <laurentpartu@gmail.com>
2026-01-11 12:11:25 +0000

    feat: update mongoose to 9.1.2 and add mongodb dependency; establish database connection in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,18 @@
+import mongoose from "mongoose";
 import app from "./app.js";
 
-const PORT = process.env.PORT ;
-const server = app.listen(PORT, ()=> console.log(`App listening on port ${PORT}`))
+const PORT = process.env.PORT;
+
+const DB_URI = process.env.DB_CONNECTION_STR!.replace(
+  "<db_password>",
+  process.env.DB_PASSWORD!
+);
+
+//Establishing db connection
+mongoose
+  .connect(DB_URI)
+  .then(() => console.log("connected to database successfully"));
+
+const server = app.listen(PORT, () =>
+  console.log(`App listening on port ${PORT}`)
+);
